f83864c027b41c620e9db1c8a13ee252310ffcd0
compfee <Bapa3Na117>
2023-04-21 03:00:17 +0300

    presentation layout corrected

--- src/example.py
+++ src/example.py
@@ -1,4 +1,6 @@
 # example
+from pptx.util import Inches
+
 from pptx_generator import PPTGenerator
 from summarization import summ_text
 from text_parsing import parse_docx
@@ -22,16 +24,24 @@ if __name__ == '__main__':
 
   ppt.add_slide(ppt.get_slide_layout(1))
   ppt.add_text(article_text_full[1]['Heading'], summ_text(article_text_full[1]), index_slide=1)
-  # ppt.add_image('Image', '1.jpg', index_slide=1)
+  ppt.add_image('Image', '1.jpg', index_slide=1)
 
   # print(article_text_full[8]['Enum'][0])
   # print(article_text_full[8]['Enum'][3])
-  list_text = article_text_full[8]['Enum'][0] + '\n' + article_text_full[8]['Enum'][2] + '\n' + article_text_full[8]['Enum'][3] + '\n' + article_text_full[8]['Enum'][4] + '\n' + article_text_full[8]['Enum'][5]
+  list_text = [article_text_full[8]['Enum'][0], article_text_full[8]['Enum'][2],
+               article_text_full[8]['Enum'][3], article_text_full[8]['Enum'][4],
+               article_text_full[8]['Enum'][5]]
   ppt.add_slide(ppt.get_slide_layout(1))
-  ppt.add_text(article_text_full[8]['Subtitle'], list_text, index_slide=2)
+  print(list_text)
+  ppt.add_text(article_text_full[8]['Subtitle'], list_text, index_slide=2, location='center')
+
 
   ppt.add_slide(ppt.get_slide_layout(1))
   ppt.add_text(article_text_full[16]['Heading'], summ_text(article_text_full[16]), index_slide=3)
+  ppt.add_image('Image', '2.jpg', index_slide=3)
+
+  # ppt.add_slide(ppt.get_slide_layout(1))
+  # ppt.add_text(article_text_full[22]['Heading'], summ_text(article_text_full[22]), index_slide=4)
 
   # ppt.add_slide(ppt.get_slide_layout(1))
   # ppt.add_text(article_text_full[16]['Heading'], summ_text(article_text_full[16]), index_slide=3)
--- src/pptx_generator.py
+++ src/pptx_generator.py
@@ -2,6 +2,7 @@
 from pptx import Presentation
 from pptx.util import Inches, Pt
 from pptx.enum.text import MSO_VERTICAL_ANCHOR, MSO_AUTO_SIZE
+from pptx.enum.text import PP_PARAGRAPH_ALIGNMENT
 import os
 # Create PPT generator class
 class PPTGenerator:
@@ -67,29 +68,77 @@ class PPTGenerator:
         title_shape.text = subtitle
 
     # Add text
-    def add_text(self, title, content, index_slide):
+    def add_text(self, title, content, index_slide, location='left'):
         slide = self.prs.slides[index_slide]
 
         title_box = slide.shapes.title
         title_box.text = title
+        # title_box.left = Inches(0.5)
+        # title_box.top = Inches(0.3)
+        # title_box.width = Inches(5)
+        # title_box.height = Inches(1)
+        if location == 'center':
+            title_box.left = Inches(2.8)
+            title_box.top = Inches(0.3)
+            title_box.width = Inches(4.5)
+            title_box.height = Inches(1)
+
+        elif location == 'left':
+            title_box.width = Inches(4.5)
+            title_box.left = Inches(0.5)
+            title_box.top = Inches(0.3)
+            title_box.height = Inches(1)
 
         title_para = slide.shapes.title.text_frame.paragraphs[0]
         title_para.font.name = "Calibri"
-        title_para.font.size = Pt(30)
-
+        title_para.font.size = Pt(18)
         title_para.font.underline = True
+
         body_shape = slide.shapes.placeholders[1]
-        text_place = body_shape.text_frame.paragraphs[0]
-        text_place.font.name = "Calibri"
-        text_place.font.size = Pt(20)
-        text_place.text = content
-        text_place.width = Inches(6)
-        text_place.height = Inches(3)
-        text_place.margin_bottom = Inches(0.08)
-        text_place.margin_left = 0
-        text_place.vertical_anchor = MSO_VERTICAL_ANCHOR.TOP
-        text_place.word_wrap = False
-        text_place.auto_size = MSO_AUTO_SIZE.SHAPE_TO_FIT_TEXT
+        if location == 'center':
+            body_shape.left = Inches(1.5)
+            body_shape.top = Inches(1.5)
+            body_shape.width = Inches(8)
+            body_shape.height = Inches(8)
+
+        elif location == 'left':
+            body_shape.width = Inches(5.0)
+            body_shape.left = Inches(0.0)
+            body_shape.top = Inches(1.5)
+            body_shape.height = Inches(8)
+
+
+        if type(content) == list:
+            for i in range(len(content)):
+
+                p = body_shape.text_frame.add_paragraph()
+                p.text = content[i]
+
+                text_place = body_shape.text_frame.paragraphs[i+1]
+                text_place.font.name = "Calibri"
+                text_place.font.size = Pt(14)
+                # text_place.text = content[i]
+                # text_place.width = Inches(5.5)
+                # text_place.height = Inches(3)
+                # text_place.margin_bottom = Inches(0.08)
+                # text_place.margin_left = 0
+                text_place.vertical_anchor = MSO_VERTICAL_ANCHOR.TOP
+                text_place.word_wrap = False
+                text_place.auto_size = MSO_AUTO_SIZE.SHAPE_TO_FIT_TEXT
+        else:
+            text_place = body_shape.text_frame.paragraphs[0]
+            text_place.font.name = "Calibri"
+            text_place.font.size = Pt(14)
+            text_place.text = content
+            # text_place.width = Inches(5.5)
+            # text_place.height = Inches(3)
+            # text_place.margin_bottom = Inches(0.08)
+            # text_place.margin_left = 0
+            text_place.vertical_anchor = MSO_VERTICAL_ANCHOR.TOP
+            text_place.word_wrap = False
+            text_place.auto_size = MSO_AUTO_SIZE.SHAPE_TO_FIT_TEXT
+            text_place.alignment = PP_PARAGRAPH_ALIGNMENT.JUSTIFY
+
 
         # left = top = Inches(2)
         # width = height = Inches(6)
@@ -105,9 +154,13 @@ class PPTGenerator:
 
     # Add image
     def add_image(self, title, image_file, index_slide):
+        pic_left = Inches(5.25)
+        pic_top = Inches(0.5)
+        pic_width = Inches(4.5)
         slide = self.get_slide(index_slide)
-        img = slide.shapes.add_picture(image_file, Inches(10.0), Inches(1.0),
-                                       width=Inches(5), height=Inches(7.5))
+        pic_height = self.prs.slide_height - Inches(1.0)
+        img = slide.shapes.add_picture(image_file, pic_left, pic_top, pic_width, pic_height)
+
 
 
     # Set background
